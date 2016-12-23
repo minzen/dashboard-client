3a18dfa4f85a3b5107d144dc46149115793a40df
marcolindner <marco.lindner@gmx.org>
2016-12-23 09:25:28 +0100

    Update app.ts

---
+++ src/server/app.ts
@@ -0,0 +1,83 @@
+import * as express from "express";
+import * as http from "http";
+import * as serveStatic from "serve-static";
+import * as path from "path";
+import * as socketIo from "socket.io";
+
+declare var process, __dirname;
+
+class Server {
+    public app: any;
+    private server: any;
+    private io: any;
+    private root: string;
+    private port: number;
+
+    // Bootstrap the application.
+    public static bootstrap(): Server {
+        return new Server();
+    }
+
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+        this.server = http.createServer(this.app);
+        this.sockets();
+        this.listen();
+    }
+
+    // Configuration
+    private config(): void {
+        // By default the port should be 5000
+        this.port = process.env.PORT || 5000;
+
+        // root path is under ../../target
+        this.root = path.join(path.resolve(__dirname, '../../target'));
+
+    }
+
+    // Configure routes
+    private routes(): void {
+        let router: express.Router;
+        router = express.Router();
+
+        // Static assets
+        this.app.use('/assets', serveStatic(path.resolve(this.root, 'assets')));
+
+        // Set router to serve index.html (e.g. single page app)
+        router.get('/', (request: express.Request, result: express.Response) => {
+            result.sendFile(path.join(this.root, '/index.html'));
+        });
+
+        // Set app to use router as the default route
+        this.app.use('*', router);
+    }
+
+    // Configure sockets
+    private sockets(): void {
+        // Get socket.io handle
+        this.io = socketIo(this.server);
+    }
+    
+    // Start HTTP server listening
+    private listen(): void {
+        //listen on provided ports
+        this.server.listen(this.port);
+
+        //add error handler
+        this.server.on("error", error => {
+            console.log("ERROR", error);
+        });
+
+        //start listening on port
+        this.server.on("listening", () => {
+            console.log('==> Listening on port %s. Open up http://localhost:%s/ in your browser.', this.port, this.port);            
+        });
+
+    }
+}
+
+// Bootstrap the server
+let server = Server.bootstrap();
+export = server.app;
