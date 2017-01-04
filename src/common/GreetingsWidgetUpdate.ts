96d8b3b09690d26162212471230b8ef884c18e73
Diemke <jdiemke@eventim.ag>
2017-01-05 00:59:46 +0100

    Added new GreetingsWidget.

---
+++ src/common/GreetingsWidgetUpdate.ts
@@ -0,0 +1,16 @@
+/**
+ * GreetingsWidgetUpdate
+ *
+ * @author Johannes Diemke <johannes.diemke@eventim.de
+ * @since 0.0.1
+ */
+
+export default class GreetingsWidgetUpdate {
+
+    public greeting: string;
+
+    constructor(greeting: string) {
+        this.greeting = greeting;
+    }
+
+}
